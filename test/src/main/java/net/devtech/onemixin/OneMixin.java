73134b78664f151ceeb236208c7988b69ec3935c
Devan-Kerman <devan@cleverpath.com>
2020-04-09 18:32:02 -0500

    basic start

---
+++ test/src/main/java/net/devtech/onemixin/OneMixin.java
@@ -0,0 +1,15 @@
+package net.devtech.onemixin;
+
+import net.devtech.onemixin.scheduler.Scheduler;
+import net.fabricmc.api.ModInitializer;
+
+public class OneMixin implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		Scheduler scheduler = new Scheduler();
+		scheduler.queue(m -> System.out.println("I am a one time task! Tick: " + m.getTicks()), 1);
+		scheduler.queue(m -> System.out.println("I am a one time task! Tick: " + m.getTicks()), 0);
+		scheduler.repeatN(m -> System.out.println("I will repeat n times! Tick: " + m.getTicks()), 1, 1, 1);
+		scheduler.repeatWhile(m -> System.out.println("I will repeat a few times"), i -> i < 15, 5, 3);
+	}
+}
